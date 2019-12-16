eac2f747fa5157a60664dcb4da4e3d36d2908cd0
rhapidfyre <maharris84@hotmail.com>
2019-12-16 15:19:06 -0800

    again

---
+++ spec/bb_spec.lua
@@ -0,0 +1,7 @@
+
+describe('bbserver', function()
+  local bbserver = require "src/[bb]/bb/main_server"
+  it('Invalid Client ID', function()
+    assert.equal(0, bbserver.CreateUniqueId())
+  end)
+end)
