4b8a69444a16bbeca8df06c6ec81a9d046cae809
rhapidfyre <maharris84@hotmail.com>
2019-12-16 13:32:50 -0800

    Setting up Automated Testing

---
+++ test.lua
@@ -0,0 +1,10 @@
+-- test
+local bbclient = require 'bb/client_main.lua'
+local bbserver = require 'bb/server_main.lua'
+--[[
+describe('bbclient', function()
+  it('adds numbers', function()
+    assert.equal(5, mylib.add(2,3))
+  end)
+end)
+]]
