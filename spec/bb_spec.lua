fd73a47578ed926cf64b8a1559ace1359cca23cd
rhapidfyre <maharris84@hotmail.com>
2019-12-16 15:23:04 -0800

    bad directory fix

--- spec/bb_spec.lua
+++ spec/bb_spec.lua
@@ -1,6 +1,6 @@
 
 describe('bbserver', function()
-  local bbserver = require "src/[bb]/bb/main_server"
+  local bbserver = require "[bb]/bb/main_server"
   it('Invalid Client ID', function()
     assert.equal(0, bbserver.CreateUniqueId(nil))
   end)
