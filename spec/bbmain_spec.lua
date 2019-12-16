83761236a7e1cab42f1b54962f22cc3e1b2e1d06
rhapidfyre <maharris84@hotmail.com>
2019-12-16 15:07:36 -0800

    try again

--- spec/bbmain_spec.lua
+++
@@ -1,7 +0,0 @@
-
-describe('bbserver', function()
-  local bbserver = require "src.[bb].bb.main_server"
-  it('Invalid Client ID', function()
-    assert.equal(0, bbserver.CreateUniqueId())
-  end)
-end)
