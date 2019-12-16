05daafea852a0913dc18b8aea57319e8ab7eca20
rhapidfyre <maharris84@hotmail.com>
2019-12-16 15:31:14 -0800

    directory fix

--- spec/bb_spec.lua
+++
@@ -1,7 +0,0 @@
-
-describe('bbserver', function()
-  local bbserver = require "src.[bb].bb.server_main"
-  it('Invalid Client ID', function()
-    assert.equal(0, bbserver.CreateUniqueId(nil))
-  end)
-end)
